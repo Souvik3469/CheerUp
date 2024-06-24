c929090e2e693ada3faed597aa70662f382aac7d
Souvik3469 <souviksen3469@gmail.com>
2024-06-24 13:44:08 +0530

    Question Ans backend tested

--- backend/src/v1/controllers/user/user.js
+++ backend/src/v1/controllers/user/user.js
@@ -26,35 +26,203 @@ const userController = {
       console.log(err, "err");
     }
   },
-  async getVendors(req, res, next) {
-  try {
-    const vendors = await prisma.user.findMany({
-      where: {
-        role: "vendor",
-      },
-    });
+  async getAllQuestionandAnswer(req, res, next) {
+    try {
+      const searchText = req.query.searchText;
+      const questions = await prisma.question.findMany({
+        where: {
+          text: {
+            contains: searchText,
+          },
+        },
+        include: {
+          answers: {
+            include: {
+              owner: true,
+            },
+          },
+          User: true,
+        },
+      });
 
-    res.json(customResponse(200, vendors));
-  } catch (err) {
-    res.json(customResponse(400, err));
-    console.log(err, "err");
-  }
-}
-,
-async getCompanies(req, res, next) {
-  try {
-    const companies = await prisma.user.findMany({
-      where: {
-        role: "company",
-      },
-    });
+      if (questions.length === 0) {
+        return res.json({
+          success: true,
+          message: [],
+        });
+      }
 
-    res.json(customResponse(200, companies));
-  } catch (err) {
-    res.json(customResponse(400, err));
-    console.log(err, "err");
-  }
-}
+      res.json({
+        success: true,
+        message: questions,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: err });
+    }
+  },
+
+  async getQuestionOfUser(req, res, next) {
+    try {
+      const userId = req.user.id;
+      const user = await prisma.user.findFirst({
+        where: {
+          id: userId,
+        },
+      });
+      if (user) {
+        // fetch all the questions along with theier answer
+        const questions = await prisma.question.findMany({
+          where: {
+            userId: userId,
+          },
+          include: {
+            answers: {
+              include: {
+                owner: true,
+              },
+            },
+          },
+        });
+        if (questions) {
+          res.json({
+            success: true,
+            message: questions,
+          });
+        } else {
+          res.json({
+            success: true,
+            message: "no question found",
+          });
+        }
+      }
+    } catch (err) {}
+  },
+  async deleteQuestion(req, res, next) {
+    try {
+      const userId = req.user.id;
+
+      if (!userId) {
+        res.json({
+          message: "User ID is missing or undefined in req.user",
+        });
+        return;
+      }
+
+      const user = await prisma.user.findFirst({
+        where: {
+          id: userId,
+        },
+      });
+
+      // Check if user exists
+      if (!user) {
+        res.json({
+          message: "User does not exist",
+        });
+        return;
+      }
+      console.log(req.params, "request");
+      const question = await prisma.question.findFirst({
+        where: {
+          id: req.params.id,
+        },
+      });
+      console.log(question);
+
+      if (!question) {
+        res.json({
+          message: "Question does not exist",
+        });
+        return;
+      }
+
+      // Check if the user owns the question
+      if (question.userId !== userId) {
+        res.json({
+          message: "Not the owner of this question",
+        });
+        return;
+      }
+
+      await prisma.question.delete({
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      res.json({
+        success: true,
+        message: "Question deleted successfully",
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  async createQuestion(req, res, next) {
+    try {
+      const { text } = req.body;
+
+      if (!text) {
+        return res.status(400).json({ error: "Text field is required." });
+      }
+
+      const question = await prisma.question.create({
+        data: {
+          text: text,
+          userId: req.user.id,
+        },
+      });
+
+      res.status(201).json({
+        success: true,
+        question,
+      });
+    } catch (err) {
+      console.error(err);
+
+      res.status(500).json({ error: err });
+    }
+  },
+  async answerQuestion(req, res, next) {
+    try {
+      const { text } = req.body;
+      const { questionId } = req.body;
+      const userId = req.user.id;
+
+      if (!text) {
+        return res.status(400).json({ error: "Text field is required." });
+      }
+
+      const existingQuestion = await prisma.question.findUnique({
+        where: {
+          id: parseInt(questionId),
+        },
+      });
+
+      if (!existingQuestion) {
+        return res.status(404).json({ error: "Question not found." });
+      }
+
+      const answer = await prisma.answer.create({
+        data: {
+          text: text,
+          questionId: parseInt(questionId),
+          userId: userId,
+        },
+      });
+
+      res.status(201).json({
+        success: true,
+        answer,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: err });
+    }
+  },
 
 };
 export default userController;
--- backend/src/v1/routes/User.route.js
+++ backend/src/v1/routes/User.route.js
@@ -9,5 +9,14 @@ import authMiddleware from "../middlewares/Auth.middleware";
 const router = express.Router();
 
 router.get("/user-details", authMiddleware, userController.userDetails);
+router.post("/create-question", authMiddleware, userController.createQuestion);
+router.post("/answer-question", authMiddleware, userController.answerQuestion);
+router.get("/user-questions", authMiddleware, userController.getQuestionOfUser);
+router.get("/get-allquestions", userController.getAllQuestionandAnswer);
+router.delete(
+  "/delete-question/:id",
+  authMiddleware,
+  userController.deleteQuestion
+);
 
 export default router;
