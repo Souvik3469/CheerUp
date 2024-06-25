dc6454965f3e3329f5911efc23e2e827874393c8
Souvik3469 <souviksen3469@gmail.com>
2024-06-25 14:05:12 +0530

    Test modal fix

--- frontend/src/app/test/test.form.tsx
+++ frontend/src/app/test/test.form.tsx
@@ -42,6 +42,16 @@ function TestForm() {
     setSelectedOptions({});
   };
 
+  const getLevelDescription = (points: number) => {
+    if (points < 150) {
+      return "Level 1: We recommend to consult the mentors or take a task.";
+    } else if (points < 230) {
+      return "Level 2: We recommend you to take a task, you can also consult a mentor.";
+    } else {
+      return "Level 3: You are doing good, still you can take a task or consult a mentor.";
+    }
+  };
+
   console.log(calculateTotalPoints(), "selected");
 
   return (
@@ -104,8 +114,9 @@ function TestForm() {
                 setOpenDialog(true);
               }}
               disabled={
+                question.data &&
                 Object.keys(selectedOptions).length !==
-                question.data[1]?.questions.length
+                  question.data[1]?.questions.length
               }
             >
               Submit
@@ -119,7 +130,16 @@ function TestForm() {
           <DialogHeader>
             <DialogTitle>Total Points</DialogTitle>
             <DialogDescription>This action cannot be undone.</DialogDescription>
-            You got {calculateTotalPoints()}
+            <p>You got {calculateTotalPoints()} points</p>
+            <p>{getLevelDescription(calculateTotalPoints())}</p>
+            <div className="flex gap-5 mt-5 pt-5">
+              <Button className="bg-green-500 hover:bg-green-600">
+                Take a Task
+              </Button>
+              <Button className="bg-yellow-500 hover:bg-yellow-600">
+                Consult Mentor
+              </Button>
+            </div>
           </DialogHeader>
         </DialogContent>
       </Dialog>
