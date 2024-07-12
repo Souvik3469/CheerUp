acf53ba2a78e4429f115b20ffdf8108ef31e3d16
loadlc2003 <banerjeeankush184@gmail.com>
2024-07-12 21:43:17 +0530

    updated

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import Sidebar from "./_components/sidebar";
-
+import { Suspense } from "react";
+import Loading from "./_components/Loader";
 interface PageProps {
   children: React.ReactNode;
 }
@@ -9,7 +10,9 @@ const Layout: FC<PageProps> = ({ children }) => {
   return (
     <div className="flex h-screen">
       <div className="flex-none hidden lg:block">
-        <Sidebar />
+        <Suspense fallback={<Loading />}>
+          <Sidebar />
+        </Suspense>
       </div>
       <div className="flex-grow overflow-auto p-1">{children}</div>
     </div>
